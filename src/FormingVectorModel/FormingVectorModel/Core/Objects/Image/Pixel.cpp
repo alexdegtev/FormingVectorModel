7d21e987b5a8da7ce5e788e8d9aa9c90885cc17f
alexdyogtev <alex.dyogtev@yandex.ru>
2016-12-11 14:30:33 +0300

    Add Pixel class

---
+++ src/FormingVectorModel/FormingVectorModel/Core/Objects/Image/Pixel.cpp
@@ -0,0 +1,63 @@
+#include "Pixel.hpp"
+
+FVM::Core::Objects::Image::Pixel::Pixel() : Pixel(0, 0, 0)
+{
+}
+
+FVM::Core::Objects::Image::Pixel::Pixel(unsigned char brightness) : Pixel(brightness, brightness, brightness)
+{
+}
+
+FVM::Core::Objects::Image::Pixel::Pixel(unsigned char red, unsigned char green, unsigned char blue) : red_(red), green_(green), blue_(blue)
+{
+}
+
+FVM::Core::Objects::Image::Pixel::Pixel(const Pixel& other) : red_(other.red_), green_(other.green_), blue_(other.blue_)
+{
+}
+
+FVM::Core::Objects::Image::Pixel FVM::Core::Objects::Image::Pixel::operator=(const Pixel& other) const
+{
+	return Pixel(other.red_, other.green_, other.blue_);
+}
+
+unsigned char FVM::Core::Objects::Image::Pixel::red() const
+{
+	return red_;
+}
+
+void FVM::Core::Objects::Image::Pixel::red(unsigned char value)
+{
+	red_ = value;
+}
+
+unsigned char FVM::Core::Objects::Image::Pixel::green() const
+{
+	return green_;
+}
+
+void FVM::Core::Objects::Image::Pixel::green(unsigned char value)
+{
+	red_ = value;
+}
+
+unsigned char FVM::Core::Objects::Image::Pixel::blue() const
+{
+	return blue_;
+}
+
+void FVM::Core::Objects::Image::Pixel::blue(unsigned char value)
+{
+	blue_ = value;
+}
+
+unsigned char FVM::Core::Objects::Image::Pixel::brightness() const
+{
+	//TODO
+	return 0;
+}
+
+void FVM::Core::Objects::Image::Pixel::brightness(unsigned char value)
+{
+	red_ = green_ = blue_ = value;
+}
---
+++ src/FormingVectorModel/FormingVectorModel/Core/Objects/Image/Pixel.hpp
@@ -0,0 +1,43 @@
+#ifndef FVM_CORE_OBJECTS_IMAGE_IIMAGE_HPP
+#define FVM_CORE_OBJECTS_IMAGE_IIMAGE_HPP
+
+namespace FVM
+{
+	namespace Core
+	{
+		namespace Objects
+		{
+			namespace Image
+			{
+				class Pixel
+				{
+				public:
+					Pixel();
+					Pixel(unsigned char brightness);
+					Pixel(unsigned char red, unsigned char green, unsigned char blue);
+					Pixel(const Pixel& other);
+
+					Pixel operator=(const Pixel& other) const;
+
+					unsigned char red() const;
+					void red(unsigned char value);
+
+					unsigned char green() const;
+					void green(unsigned char value);
+
+					unsigned char blue() const;
+					void blue(unsigned char value);
+
+					unsigned char brightness() const;
+					void brightness(unsigned char value);
+
+				private:
+					unsigned char red_;
+					unsigned char green_;
+					unsigned char blue_;
+				};
+			}
+		}
+	}
+}
+#endif
